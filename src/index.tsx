fdc1a15c5b4d6abe1fbd56af15ce005642f1dac4
Victor-maestro <victor_maestro@mail.ru>
2024-01-07 19:33:41 +0600

    2.10 Добавил компоненты

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const CARDS_COUNT = 6;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App cardsCount={CARDS_COUNT} />
   </React.StrictMode>,
 );
